a30f441fb552739211f84f01cb8d351f0d503105
unknown <bshsf@qq.com>
2018-07-16 03:49:39 +0800

    新增Json,生成json实体类，反序列化并获得图片url并访问

---
+++ Assets/ReadInfoVarJson.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QuickType;
+using UnityEngine.UI;
+using System;
+using QFramework.TimeExtend;
+public class ReadInfoVarJson : MonoBehaviour
+{
+    public TextAsset jsonData;
+    public Image image; //显示封面
+    public Text text;//显示第几集
+    public Button button;//点击使用浏览器访问选中的集数
+
+    private Coroutine cachedCor;
+    private VideoData data;
+    private Vlist[] vlist;
+    private int index = 0;
+
+    void Start()
+    {
+        data = VideoData.FromJson(jsonData.text);
+        vlist = data.Data.Vlist;
+    }
+
+
+
+    IEnumerator GetCoverVarUrl(string url, Action<Sprite> OnComplected)
+    {
+        WWW www = new WWW(url);
+        Timer.AddTimer(1, "ForCor").OnCompleted(() =>  //使用定时器安放一个超时检测
+        {
+            Debug.LogWarning("连接超时！");
+            www.Dispose();
+            StopCoroutine(cachedCor);
+            cachedCor = null;
+        });
+        yield return www;
+        if (www.isDone && string.IsNullOrEmpty(www.error))
+        {
+            Timer.DelTimer("ForCor"); //只要访问完成，就将定时器销毁，停止计时检测
+            Texture2D texture = new Texture2D(0, 0);
+            www.LoadImageIntoTexture(texture);
+            if (null != texture)
+            {
+                Rect rect = new Rect(Vector2.zero, new Vector2(texture.width, texture.height));
+                Sprite sprite = Sprite.Create(texture, rect, Vector2.zero);
+                if (null != OnComplected)
+                {
+                    OnComplected(sprite);
+                }
+            }
+        }
+        www.Dispose();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (null == cachedCor)
+            {
+                if (index > vlist.Length - 1)
+                {
+                    Debug.LogWarning("显示完毕了呀");
+                    index = 0; //从头开始
+                    return;
+                }
+                string picUrl = vlist[index].Vpic;
+                string msg = string.Format("{0}\n<size=14>{1}</size>", vlist[index].ShortTitle, vlist[index].Vt);
+                string videoUrl = vlist[index].Vurl;
+
+
+                cachedCor = StartCoroutine(GetCoverVarUrl(picUrl, (V) =>
+                {
+                    text.text = msg; //显示第几集
+                    image.sprite = V; //显示封面
+                    image.SetNativeSize();
+                    cachedCor = null;
+                    button.GetComponentInChildren<Text>().text = vlist[index].ShortTitle.Split(' ')[1]; //按钮加个标题
+                    button.onClick.RemoveAllListeners(); //移除所有的监听
+                    button.onClick.AddListener(() =>     //开启新的监听
+                    {
+                        System.Diagnostics.Process.Start(videoUrl);  //开启这个链接
+                        //System.Diagnostics.Process.Start(vlist[index].Vurl);  //BUG语句 闭包在这里会导致访问的是下一集的链接，因为这个Action里面使用了index，而他随后被自增，所以当点击事件发生时，index已经被加1，故而它访问的是下一条链接，解决办法就是，使用局部变量“videoUrl”缓存这条数据即可
+                     });
+                    index++;
+                }));
+            }
+            else
+            {
+                Debug.LogWarning("点击频率过快！");
+            }
+        }
+    }
+}
---
+++ Assets/VideoData.cs
@@ -0,0 +1,275 @@
+﻿// To parse this JSON data, add NuGet 'Newtonsoft.Json' then do:
+//
+//    using QuickType;
+//
+//    var videoData = VideoData.FromJson(jsonString);
+
+namespace QuickType
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class VideoData
+    {
+        [JsonProperty("bmsg")]
+        public Bmsg Bmsg { get; set; }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("data")]
+        public Data Data { get; set; }
+
+        [JsonProperty("timestamp")]
+        public string Timestamp { get; set; }
+    }
+
+    public partial class Bmsg
+    {
+        [JsonProperty("t")]
+        public string T { get; set; }
+
+        [JsonProperty("f")]
+        public string F { get; set; }
+
+        [JsonProperty("mid")]
+        public string Mid { get; set; }
+
+        [JsonProperty("sp")]
+        public string Sp { get; set; }
+    }
+
+    public partial class Data
+    {
+        [JsonProperty("pp")]
+        public long Pp { get; set; }
+
+        [JsonProperty("ps")]
+        public string Ps { get; set; }
+
+        [JsonProperty("pt")]
+        public long Pt { get; set; }
+
+        [JsonProperty("vlist")]
+        public Vlist[] Vlist { get; set; }
+
+        [JsonProperty("bossType")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long BossType { get; set; }
+
+        [JsonProperty("aQipuId")]
+        public long AQipuId { get; set; }
+
+        [JsonProperty("qiyiProduced")]
+        public long QiyiProduced { get; set; }
+
+        [JsonProperty("allNum")]
+        public long AllNum { get; set; }
+
+        [JsonProperty("pg")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Pg { get; set; }
+
+        [JsonProperty("isBossMixer")]
+        public long IsBossMixer { get; set; }
+
+        [JsonProperty("ic")]
+        public long Ic { get; set; }
+
+        [JsonProperty("pgt")]
+        public long Pgt { get; set; }
+
+        [JsonProperty("aid")]
+        public long Aid { get; set; }
+
+        [JsonProperty("pm")]
+        public long Pm { get; set; }
+
+        [JsonProperty("pn")]
+        public long Pn { get; set; }
+
+        [JsonProperty("cid")]
+        public long Cid { get; set; }
+    }
+
+    public partial class Vlist
+    {
+        [JsonProperty("mdown")]
+        public long Mdown { get; set; }
+
+        [JsonProperty("wmarkPos")]
+        public long WmarkPos { get; set; }
+
+        [JsonProperty("publishTime")]
+        public long PublishTime { get; set; }
+
+        [JsonProperty("vpic")]
+        public string Vpic { get; set; }
+
+        [JsonProperty("tvQipuId")]
+        [JsonConverter(typeof(DecodingChoiceConverter))]
+        public long TvQipuId { get; set; }
+
+        [JsonProperty("payMarkUrl")]
+        public string PayMarkUrl { get; set; }
+
+        [JsonProperty("purType")]
+        public long PurType { get; set; }
+
+        [JsonProperty("shortTitle")]
+        public string ShortTitle { get; set; }
+
+        [JsonProperty("type")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Type { get; set; }
+
+        [JsonProperty("lgh")]
+        public object[] Lgh { get; set; }
+
+        [JsonProperty("isProduced")]
+        public long IsProduced { get; set; }
+
+        [JsonProperty("vurl")]
+        public string Vurl { get; set; }
+
+        [JsonProperty("plcdown")]
+        public Dictionary<string, long> Plcdown { get; set; }
+
+        [JsonProperty("vid")]
+        public string Vid { get; set; }
+
+        [JsonProperty("timeLength")]
+        public long TimeLength { get; set; }
+
+        [JsonProperty("pd")]
+        public long Pd { get; set; }
+
+        [JsonProperty("vn")]
+        public string Vn { get; set; }
+
+        [JsonProperty("payMark")]
+        public long PayMark { get; set; }
+
+        [JsonProperty("exclusive")]
+        public long Exclusive { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("vt")]
+        public string Vt { get; set; }
+
+        [JsonProperty("pds")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Pds { get; set; }
+    }
+
+    public partial class VideoData
+    {
+        public static VideoData FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<VideoData>(json, QuickType.Converter.Settings);
+        }
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this VideoData self)
+        {
+            return JsonConvert.SerializeObject(self, QuickType.Converter.Settings);
+        }
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    internal class ParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t)
+        {
+            return t == typeof(long) || t == typeof(long?);
+        }
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            long l;
+            if (Int64.TryParse(value, out l))
+            {
+                return l;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value.ToString());
+            return;
+        }
+
+        public static readonly ParseStringConverter Singleton = new ParseStringConverter();
+    }
+
+    internal class DecodingChoiceConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t)
+        {
+            return t == typeof(long) || t == typeof(long?);
+        }
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            switch (reader.TokenType)
+            {
+                case JsonToken.Integer:
+                    var integerValue = serializer.Deserialize<long>(reader);
+                    return integerValue;
+                case JsonToken.String:
+                case JsonToken.Date:
+                    var stringValue = serializer.Deserialize<string>(reader);
+                    long l;
+                    if (Int64.TryParse(stringValue, out l))
+                    {
+                        return l;
+                    }
+                    break;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value);
+            return;
+        }
+
+        public static readonly DecodingChoiceConverter Singleton = new DecodingChoiceConverter();
+    }
+}
